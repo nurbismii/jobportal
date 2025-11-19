685d38243346e3937f17b772322b705bcc7fb731
Nurbismii <nurbismi74@gmail.com>
2025-11-19 13:55:36 +0800

    feat: schedule delete unverified pengguna

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // Jalankan tiap jam, atau bisa setiap 15 menit
+        $schedule->command('users:cleanup-unverified')->everyMinute();
     }
 
     /**
@@ -25,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
