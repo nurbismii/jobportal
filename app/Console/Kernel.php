2fb23ebc8dc64e4833babff500891113cc1a04c7
Nurbismii <nurbismi74@gmail.com>
2025-11-21 10:24:17 +0800

    fix: schedule run delete user

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,10 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
+    protected $commands = [
+        \App\Console\Commands\DeleteUnverifiedUsers::class,
+    ];
+
     protected function schedule(Schedule $schedule)
     {
         // Jalankan tiap jam, atau bisa setiap 15 menit
