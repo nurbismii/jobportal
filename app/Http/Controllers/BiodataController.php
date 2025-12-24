53690fa54296aeb6ecc90d62a7fd3f463d0b3678
Nurbismii <nurbismi74@gmail.com>
2025-12-24 15:54:50 +0800

    test: catch gagal upload dokumen

--- app/Http/Controllers/BiodataController.php
+++ app/Http/Controllers/BiodataController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Intervention\Image\Facades\Image;
 
 class BiodataController extends Controller
@@ -30,122 +31,128 @@ class BiodataController extends Controller
 
     public function store(Request $request)
     {
-        $biodata = Biodata::where('user_id', auth()->id())->first();
-        $syarat_ketentuan = SyaratKetentuan::where('id', 1)->first();
+        try {
+            $biodata = Biodata::where('user_id', auth()->id())->first();
+            $syarat_ketentuan = SyaratKetentuan::where('id', 1)->first();
 
-        $dokumenFields = [
-            'cv' => 'CV',
-            'pas_foto' => 'Pas Foto',
-            'surat_lamaran' => 'Surat Lamaran',
-            'ijazah' => 'Ijazah',
-            'ktp' => 'KTP',
-            'sim_b_2' => 'SIM B II Umum',
-            'sio' => 'Surat Izin Operator',
-            'skck' => 'SKCK',
-            'sertifikat_vaksin' => 'Sertifikat Vaksin',
-            'kartu_keluarga' => 'Kartu Keluarga',
-            'npwp' => 'NPWP',
-            'ak1' => 'Kartu AK1',
-            'sertifikat_pendukung' => 'Sertifikat Pendukung'
-        ];
+            $dokumenFields = [
+                'cv' => 'CV',
+                'pas_foto' => 'Pas Foto',
+                'surat_lamaran' => 'Surat Lamaran',
+                'ijazah' => 'Ijazah',
+                'ktp' => 'KTP',
+                'sim_b_2' => 'SIM B II Umum',
+                'sio' => 'Surat Izin Operator',
+                'skck' => 'SKCK',
+                'sertifikat_vaksin' => 'Sertifikat Vaksin',
+                'kartu_keluarga' => 'Kartu Keluarga',
+                'npwp' => 'NPWP',
+                'ak1' => 'Kartu AK1',
+                'sertifikat_pendukung' => 'Sertifikat Pendukung'
+            ];
 
-        $fileNames = [];
+            $fileNames = [];
 
-        $fileNames = interventionImg($dokumenFields, $biodata, $request);
+            $fileNames = interventionImg($dokumenFields, $biodata, $request);
 
-        $fileNames = $fileNames['files'];
-        $oldFiles  = $fileNames['oldFiles'] ?? [];
+            $fileNames = $fileNames['files'];
+            $oldFiles  = $fileNames['oldFiles'] ?? [];
 
-        $biodata = Biodata::updateOrCreate(
-            [
-                'user_id' => auth()->id()
-            ],
-            [
-                // Biodata Pribadi
-                'user_id' => auth()->id(),
-                'no_ktp' => $request->no_ktp,
-                'no_telp' => $request->no_telp,
-                'no_kk' => $request->no_kk,
-                'no_npwp' => $request->no_npwp,
-                'jenis_kelamin' => $request->jenis_kelamin,
-                'tempat_lahir' => $request->tempat_lahir,
-                'tanggal_lahir' => $request->tanggal_lahir,
-                'agama' => $request->agama,
-                'vaksin' => $request->vaksin,
-                'provinsi' => $request->provinsi,
-                'kabupaten' => $request->kabupaten,
-                'kecamatan' => $request->kecamatan,
-                'kelurahan' => $request->kelurahan,
-                'alamat' => $request->alamat,
-                'alamat_domisili' => $request->alamat_domisili,
-                'kode_pos' => $request->kode_pos,
-                'rt' => $request->rt,
-                'rw' => $request->rw,
-                'hobi' => $request->hobi,
-                'golongan_darah' => $request->golongan_darah,
-                'tinggi_badan' => $request->tinggi_badan,
-                'berat_badan' => $request->berat_badan,
+            $biodata = Biodata::updateOrCreate(
+                [
+                    'user_id' => auth()->id()
+                ],
+                [
+                    // Biodata Pribadi
+                    'user_id' => auth()->id(),
+                    'no_ktp' => $request->no_ktp,
+                    'no_telp' => $request->no_telp,
+                    'no_kk' => $request->no_kk,
+                    'no_npwp' => $request->no_npwp,
+                    'jenis_kelamin' => $request->jenis_kelamin,
+                    'tempat_lahir' => $request->tempat_lahir,
+                    'tanggal_lahir' => $request->tanggal_lahir,
+                    'agama' => $request->agama,
+                    'vaksin' => $request->vaksin,
+                    'provinsi' => $request->provinsi,
+                    'kabupaten' => $request->kabupaten,
+                    'kecamatan' => $request->kecamatan,
+                    'kelurahan' => $request->kelurahan,
+                    'alamat' => $request->alamat,
+                    'alamat_domisili' => $request->alamat_domisili,
+                    'kode_pos' => $request->kode_pos,
+                    'rt' => $request->rt,
+                    'rw' => $request->rw,
+                    'hobi' => $request->hobi,
+                    'golongan_darah' => $request->golongan_darah,
+                    'tinggi_badan' => $request->tinggi_badan,
+                    'berat_badan' => $request->berat_badan,
 
-                // Pendidikan
-                'pendidikan_terakhir' => $request->pendidikan_terakhir,
-                'nama_instansi' => ucwords($request->nama_instansi),
-                'jurusan' => ucwords($request->jurusan),
-                'nilai_ipk' => $request->nilai_ipk,
-                'tahun_masuk' => $request->tahun_masuk,
-                'tahun_lulus' => $request->tahun_lulus,
-                'prestasi' => $request->prestasi,
+                    // Pendidikan
+                    'pendidikan_terakhir' => $request->pendidikan_terakhir,
+                    'nama_instansi' => ucwords($request->nama_instansi),
+                    'jurusan' => ucwords($request->jurusan),
+                    'nilai_ipk' => $request->nilai_ipk,
+                    'tahun_masuk' => $request->tahun_masuk,
+                    'tahun_lulus' => $request->tahun_lulus,
+                    'prestasi' => $request->prestasi,
 
-                // Keluarga
-                'nama_ayah' => ucwords($request->nama_ayah),
-                'nama_ibu' => ucwords($request->nama_ibu),
-                'status_pernikahan' => $request->status_pernikahan,
-                'tanggal_nikah' => $request->tanggal_nikah,
-                'nama_pasangan' => ucwords($request->nama_pasangan),
-                'jumlah_anak' => $request->jumlah_anak,
-                'nama_anak_1' => $request->nama_anak_1,
-                'nama_anak_2' => $request->nama_anak_2,
-                'nama_anak_3' => $request->nama_anak_3,
+                    // Keluarga
+                    'nama_ayah' => ucwords($request->nama_ayah),
+                    'nama_ibu' => ucwords($request->nama_ibu),
+                    'status_pernikahan' => $request->status_pernikahan,
+                    'tanggal_nikah' => $request->tanggal_nikah,
+                    'nama_pasangan' => ucwords($request->nama_pasangan),
+                    'jumlah_anak' => $request->jumlah_anak,
+                    'nama_anak_1' => $request->nama_anak_1,
+                    'nama_anak_2' => $request->nama_anak_2,
+                    'nama_anak_3' => $request->nama_anak_3,
 
-                // Kontak darurat
-                'nama_kontak_darurat' => ucwords($request->nama_kontak_darurat),
-                'no_telepon_darurat' => $request->no_telp_darurat,
-                'status_hubungan' => $request->status_hubungan,
+                    // Kontak darurat
+                    'nama_kontak_darurat' => ucwords($request->nama_kontak_darurat),
+                    'no_telepon_darurat' => $request->no_telp_darurat,
+                    'status_hubungan' => $request->status_hubungan,
 
-                // Dokumen (diambil dari array fileNames)
-                'cv' => $fileNames['cv'],
-                'pas_foto' => $fileNames['pas_foto'],
-                'surat_lamaran' => $fileNames['surat_lamaran'],
-                'ijazah' => $fileNames['ijazah'],
-                'ktp' => $fileNames['ktp'],
-                'sim_b_2' => $fileNames['sim_b_2'],
-                'sio' => $fileNames['sio'],
-                'skck' => $fileNames['skck'],
-                'sertifikat_vaksin' => $fileNames['sertifikat_vaksin'],
-                'kartu_keluarga' => $fileNames['kartu_keluarga'],
-                'npwp' => $fileNames['npwp'],
-                'ak1' => $fileNames['ak1'],
-                'sertifikat_pendukung' => $fileNames['sertifikat_pendukung'],
+                    // Dokumen (diambil dari array fileNames)
+                    'cv' => $fileNames['cv'],
+                    'pas_foto' => $fileNames['pas_foto'],
+                    'surat_lamaran' => $fileNames['surat_lamaran'],
+                    'ijazah' => $fileNames['ijazah'],
+                    'ktp' => $fileNames['ktp'],
+                    'sim_b_2' => $fileNames['sim_b_2'],
+                    'sio' => $fileNames['sio'],
+                    'skck' => $fileNames['skck'],
+                    'sertifikat_vaksin' => $fileNames['sertifikat_vaksin'],
+                    'kartu_keluarga' => $fileNames['kartu_keluarga'],
+                    'npwp' => $fileNames['npwp'],
+                    'ak1' => $fileNames['ak1'],
+                    'sertifikat_pendukung' => $fileNames['sertifikat_pendukung'],
 
-                // Tambahan
-                'status_pernyataan' => $syarat_ketentuan->syarat_ketentuan
-            ]
-        );
+                    // Tambahan
+                    'status_pernyataan' => $syarat_ketentuan->syarat_ketentuan
+                ]
+            );
 
-        foreach ($oldFiles as $oldFile) {
-            $path = public_path(auth()->user()->no_ktp . '/dokumen/' . $oldFile);
-            if (is_file($path)) {
-                unlink($path);
+            foreach ($oldFiles as $oldFile) {
+                $path = public_path(auth()->user()->no_ktp . '/dokumen/' . $oldFile);
+                if (is_file($path)) {
+                    unlink($path);
+                }
             }
-        }
 
-        // Check if SIM B II file is available before processing OCR
-        if (isset($fileNames['sim_b_2']) && $fileNames['sim_b_2']) {
-            // Langsung proses OCR saat file diunggah
-            extractSimB2OnlyOCR($biodata);
-        }
+            // Check if SIM B II file is available before processing OCR
+            if (isset($fileNames['sim_b_2']) && $fileNames['sim_b_2']) {
+                // Langsung proses OCR saat file diunggah
+                extractSimB2OnlyOCR($biodata);
+            }
 
-        Alert::success('success', 'Biodata sudah diubah. Silakan pilih lowongan dan kirim lamaran');
-        return redirect()->to(route('lowongan-kerja.index'));
+            Alert::success('success', 'Biodata sudah diubah. Silakan pilih lowongan dan kirim lamaran');
+            return redirect()->to(route('lowongan-kerja.index'));
+        } catch (\Exception $e) {
+            Alert::error('Error', 'Terjadi kesalahan, coba beberapa saat lagi');
+            Log::info('BiodataController Store Error: ' . $e->getMessage());
+            return redirect()->back()->withInput();
+        }
     }
 
     public function deleteFile($field)
