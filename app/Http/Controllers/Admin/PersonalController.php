56e6fbfbc1a4f5f604acb6ee0650c00e9bcf38ad
Nurbismii <nurbismi74@gmail.com>
2026-01-16 09:50:40 +0800

    test: hanya menampilkan data personal tertentu

--- app/Http/Controllers/Admin/PersonalController.php
+++ app/Http/Controllers/Admin/PersonalController.php
@@ -9,7 +9,9 @@ class PersonalController extends Controller
 {
     public function index()
     {
-        $biodata = Biodata::with('user')->get();
+        $biodata = Biodata::with('user')->whereHas('user', function ($query) {
+            $query->where('status_akun', 1);
+        })->get();
 
         return view('admin.personal-file.index', compact('biodata'))->with('no');
     }
