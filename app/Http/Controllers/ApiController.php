75dbc3fc4439d3d455207d0ba8318c141a63b4f9
Nurbismii <nurbismi74@gmail.com>
2026-01-16 09:43:54 +0800

    test: remove debug parsing ocr sim

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -81,8 +81,6 @@ class ApiController extends Controller
 
             // Gunakan parser
             $parsedResult = app()->call([new \App\Http\Controllers\LowonganController, 'parseSimB2'], ['biodata' => $biodata, 'save' => false]);
-            
-            Log::info('PARSE SIM B2 RESULT', $parsedResult);
 
             return response()->json([
                 'success' => true,
@@ -90,8 +88,7 @@ class ApiController extends Controller
             ]);
         } catch (\Throwable $e) {
             Log::info('OCR SIM B2 Error: ' . $e->getMessage());
-
-            return response()->json(['success' => false, 'message' => 'Terjadi kesalahan saat OCR.']);
+            return response()->json(['success' => false, 'message' => 'Terjadi kesalahan, coba beberapa saat lagi.']);
         } finally {
             \Storage::disk('public')->delete($path);
         }
