439238b1f1b3f49526a3a2f3ccd96f9142e71aa9
Nurbismii <nurbismi74@gmail.com>
2026-07-06 14:13:02 +0800

    test: ganti nama button tutup menjadi kembali with icon

--- resources/views/user/lamaran/show.blade.php
+++ resources/views/user/lamaran/show.blade.php
@@ -18,7 +18,7 @@
                 <div class="card-body">
                     <div class="d-flex align-items-center mb-3">
                         <h4 class="fw-bold mb-0">Detail Lowongan</h4>
-                        <a href="{{ route('lamaran.index') }}" class="ms-auto btn btn-link text-decoration-none">Kembali</a>
+                        <a href="{{ route('lamaran.index') }}" class="ms-auto btn btn-link text-decoration-none"><i class="fa fa-arrow-left me-2"></i>Kembali</a>
 
                     </div>
                     <p class="mb-1"><strong>Posisi:</strong> {{ $lamaran->lowongan->nama_lowongan }}</p>
